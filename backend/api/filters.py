e9c3a23ed7fa7eb2ec90284fdfa7d1936cb6fbd3
captainKoala <konstantin.paulkin@gmail.com>
2021-08-09 04:08:29 +0300

    Исправлена фильтрация рецептов по тегам

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -8,10 +8,8 @@ class RecipeFilter(filters.FilterSet):
                                          method="get_is_favorited")
     is_in_shopping_cart = filters.BooleanFilter(
         field_name="to_shopping",
-        method="get_is_in_shopping_cart",
-    )
-    tags = filters.CharFilter(field_name="tags__slug",
-                              lookup_expr="exact")
+        method="get_is_in_shopping_cart")
+    tags = filters.CharFilter(method="get_tags")
 
     class Meta:
         model = Recipe
@@ -22,11 +20,16 @@ class RecipeFilter(filters.FilterSet):
         if value:
             return (Recipe.objects.filter(favourites__user=user)
                     .order_by("-pub_date"))
-        return Recipe.objects.all().order_by("-pub_date")
+        return Recipe.objects.order_by("-pub_date")
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value:
             return (Recipe.objects.filter(to_shopping__user=user)
                     .order_by("-pub_date"))
-        return Recipe.objects.all().order_by("-pub_date")
+        return Recipe.objects.order_by("-pub_date")
+
+    def get_tags(self, queryset, name, value):
+        params = self.request.query_params.getlist("tags")
+        return (Recipe.objects.filter(tags__slug__in=params)
+                .distinct().order_by("-pub_date"))
