1c5483f9118ec9fc41eea55f395cf644cefedcc8
captainKoala <konstantin.paulkin@gmail.com>
2021-08-10 21:19:18 +0300

    Приложение website: Переписаны вью-функции для страницы со списком рецептов. Реализован механим подписки на автора. Добавлены страницы автора и избранного. В шапку добавлены счетчики избранного и списка покупок

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -3,19 +3,21 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import (index, signup, WebsiteRecipeViewSet,
-                    WebsiteRecipeFavouriteViewSet, WebsiteShoppingCartViewSet)
+from .views import (author_page, favorite_recipes, index, recipe_single, recipes_list, signup,
+                    WebsiteRecipeFavouriteViewSet, WebsiteShoppingCartViewSet,
+                    WebsiteSubscriptionsViewSet)
 
 
 router = DefaultRouter()
-
-router.register("recipes", WebsiteRecipeViewSet, basename="web-recipes")
 
 urlpatterns = [
     path("", index, name="index"),
     path("signup/", signup, name="signup"),
     path("login/", views.LoginView.as_view(), name="web-login"),
     path("logout/", views.LogoutView.as_view(), name="web-logout"),
+    path("favorite/", favorite_recipes, name="web-favorite-recipes"),
+    path("recipes/", recipes_list, name="web-recipes"),
+    path("recipes/<int:recipe_id>/", recipe_single, name="web-recipe-single"),
     path('recipes/<int:recipe_id>/favorite/',
          WebsiteRecipeFavouriteViewSet.as_view({'get': 'create',
                                                 'delete': 'destroy'}),
@@ -24,5 +26,9 @@ urlpatterns = [
          WebsiteShoppingCartViewSet.as_view({'get': 'create',
                                              'delete': 'destroy'}),
          name='web_recipes_shopping_cart'),
-    path("", include(router.urls), name="api_v1"),
+    path("users/<int:author_id>/", author_page, name="web-author-page"),
+    path('users/<int:author_id>/subscribe/',
+         WebsiteSubscriptionsViewSet.as_view({'get': 'create',
+                                              'delete': 'destroy'}),
+         name='web_recipes_shopping_cart'),
 ]
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,93 +1,111 @@
 from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
 
 from api.models import Recipe, Tag
-from api.views import RecipeViewSet, RecipeFavouriteViewSet, ShoppingCartViewSet, TagViewSet
-from .forms import CreationForm
+from api.views import (RecipeFavouriteViewSet, ShoppingCartViewSet,
+                       SubscriptionsViewSet)
+from users.models import User
 
 
 def index(request):
     """ Главная страница сайта. """
     if request.user.is_authenticated:
-        return redirect(reverse("web-recipes-list"))
+        return redirect(reverse("web-recipes"))
     return redirect(reverse("web-login"))
 
 
 def signup(request):
     """Страница регистрации пользователя."""
     if request.method == "POST":
-        print("\n"*5)
-        url = "http://" + str(get_current_site(request)) + reverse("user-list")
-        print(url)
-
-        request_data = request.POST
-        data = {}
-        data["username"] = request_data["username"]
-        data["first_name"] = request_data["first_name"]
-        data["last_name"] = request_data["last_name"]
-        data["email"] = request_data["email"]
-        # data["password"] = request_data["password"]
-
-        print(data)
-        # headers = {
-        #     'Content-Type': 'application/json'
-        # }
-        # response = requests.request("POST", url, headers=headers, data=data)
-        # print(response.json())
-
-    form = CreationForm()
-    context = {
-        "form": form,
-    }
+        pass
     return render(request, "signup.html", context=context)
 
 
-class WebsiteRecipeViewSet(RecipeViewSet):
-    """Управление рецептами"""
-    renderer_classes = (TemplateHTMLRenderer,)
-    authentication_classes = (SessionAuthentication, )
-
-    def retrieve(self, request, *args, **kwargs):
-        # instance = self.get_object()
-        # serializer = self.get_serializer(instance)
-        # return Response(serializer.data)
-        response = super().retrieve(self, request, *args, **kwargs)
-        recipe = response.data
-        context = {"recipe": recipe}
-        return Response(context, template_name="recipe-single.html")
-
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-        selected_tags = request.query_params.getlist("tags")
-        is_favorited = request.query_params.get("is_favorited")
-
-        title = "Избранное" if is_favorited else "Рецепты"
-
-        if not selected_tags:
-            selected_tags = ("__all__", )
-
-        if request.accepted_renderer.format == "html":
-            tags = Tag.objects.all()
-            data = response.data
-            recipes = data.get("results")
-            context = {
-                "recipes": recipes,
-                "tags": tags,
-                "selected_tags": selected_tags,
-                "title": title,
-            }
-            return Response(context, template_name="recipes.html")
-        return response
-
-
 class WebsiteRecipeFavouriteViewSet(RecipeFavouriteViewSet):
+    """Добавление и удаление рецепта из избранного."""
     authentication_classes = (SessionAuthentication, )
 
 
 class WebsiteShoppingCartViewSet(ShoppingCartViewSet):
+    """Добавление и удаление рецепта из списка покупок."""
     authentication_classes = (SessionAuthentication, )
+
+
+class WebsiteSubscriptionsViewSet(SubscriptionsViewSet):
+    """Добавление и удаление подписки на автора."""
+    authentication_classes = (SessionAuthentication, )
+
+
+def create_context(request):
+    """Извлечение параметров из запроса и формирование контекста для ответа."""
+    selected_tags = request.GET.getlist("tags")
+
+    tags = Tag.objects.all()
+    if not selected_tags:
+        selected_tags = tuple(tag.slug for tag in tags)
+
+    if request.user.is_authenticated:
+        shopping_cart = Recipe.objects.filter(to_shopping__user=request.user)
+        favorite_recipes = Recipe.objects.filter(favourites__user=request.user)
+    else:
+        shopping_cart = favorite_recipes = 0
+    return {
+        "favorite_recipes": favorite_recipes,
+        "selected_tags": selected_tags,
+        "shopping_cart": shopping_cart,
+        "tags": tags,
+    }
+
+
+def recipe_single(request, recipe_id):
+    """Страница с одним рецептом."""
+    context = create_context(request)
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    context["recipe"] = recipe
+    return render(request, context=context, template_name="recipe-single.html")
+
+
+def recipes_list(request):
+    """Страница со списком рецептов."""
+    context = create_context(request)
+
+    recipes = (Recipe.objects.none()
+               if "__none__" in context["selected_tags"] else
+               Recipe.objects.filter(tags__slug__in=context["selected_tags"])
+               .distinct().order_by("-pub_date"))
+
+    context["recipes"] = recipes
+    return render(request, context=context, template_name="recipes.html")
+
+
+def favorite_recipes(request):
+    """Страница с избранными рецептами."""
+    context = create_context(request)
+
+    recipes = (Recipe.objects.none()
+               if "__none__" in context["selected_tags"] else
+               context["favorite_recipes"].filter(tags__slug__in=context["selected_tags"])
+               .distinct().order_by("-pub_date"))
+
+    context["recipes"] = recipes
+    return render(request, context=context, template_name="recipes.html")
+
+
+def author_page(request, author_id):
+    """Страница автора рецепта."""
+    context = create_context(request)
+
+    author = get_object_or_404(User, id=author_id)
+    recipes = (Recipe.objects.none()
+               if "__none__" in context["selected_tags"] else
+               author.recipes.filter(tags__slug__in=context["selected_tags"])
+               .distinct().order_by("-pub_date"))
+    is_followed = author in request.user.follows.all()
+
+    context["author"] = author
+    context["recipes"] = recipes
+    context["is_followed"] = is_followed
+    return render(request, context=context, template_name="author.html")
